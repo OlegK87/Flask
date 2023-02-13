7a2d364c87dcc6352497754d932c7e1957d41f1a
password <moto198720@yandex.ru>
2023-02-13 20:50:08 +0300

    ДЗ

--- client2.py
+++ client2.py
@@ -1,7 +1,7 @@
 import requests
 
-# response = requests.post('http://127.0.0.1:5001/advertisement', json={'header': 'job', 'descriptions': 'super',
-#                                                                       'owner': '1'})
+response = requests.post('http://127.0.0.1:5001/advertisement', json={'header': 'job', 'descriptions': 'super',
+                                                                      'owner': '1'})
 
 
 # response = requests.get('http://127.0.0.1:5001/advertisement/1')
